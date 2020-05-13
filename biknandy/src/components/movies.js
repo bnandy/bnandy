d77c355410e0fc2f094d02537155e0febc6b9f0a
Bik Nandy <bikramn123@gmail.com>
2020-05-13 16:25:44 -0700

    Added favorite movies and completed axios request

--- biknandy/src/components/movies.js
+++ biknandy/src/components/movies.js
@@ -1,12 +1,34 @@
 import React, { Component, useEffect, useState} from 'react';
-import {Container, Row, Col, Form, Button, Card} from 'react-bootstrap';
+import {Container, Row, Col, Form, Button, Card, CardGroup} from 'react-bootstrap';
+import movies from "../movies.json";
+import axios from 'axios';
+
+  //ed114b73 - API KEY
 
 export default function Movies() {
-
-  const [data, setData] = useState(null)
+  const [completeMovieData, setMovieData] = useState([])
   const [rend, setRend] = useState(true)
 
+  var promises = [];
+  var movieData = [];
 
+
+  //retrieve all information from axios
+  useEffect(() => {
+    Object.values(movies).forEach((movieID) => {
+      var url = "http://www.omdbapi.com/?apikey=ed114b73&i="+movieID;
+      promises.push (axios.get(url));
+    });
+
+    axios.all(promises).then((results) => {
+      results.forEach((res) => {
+          movieData.push(res.data)
+
+      })
+      setMovieData(movieData)
+    });
+  });
+  console.log(completeMovieData)
   return(
     <div>
       <Container fluid>
@@ -16,7 +38,15 @@ export default function Movies() {
           </Col>
         </Row>
         <Row>
-          
+          <CardGroup>
+            {completeMovieData.map((movieObj)=> (
+              <Card>
+                <Card.Body>
+                  {movieObj.Title}
+                </Card.Body>
+              </Card>
+            ))}
+          </CardGroup>
         </Row>
       </Container>
     </div>
