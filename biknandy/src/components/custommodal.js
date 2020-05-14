fe7d4005ef3aa9e808ec9bc31b983dcc008b69aa
Bik Nandy <bikramn123@gmail.com>
2020-05-13 19:14:11 -0700

    completed main functionality

--- biknandy/src/components/custommodal.js
+++ biknandy/src/components/custommodal.js
@@ -3,17 +3,26 @@ import {Container, Row, Col, Form, Button, Card, Image, Modal} from 'react-boots
 
 
 export default function CustomModal(props) {
-
+  console.log(props.movieData)
   return(
     <Modal show={props.show} onHide={props.handleClose} animation={false}>
       <Modal.Header closeButton>
         <Modal.Title>{props.movieData.Title}</Modal.Title>
       </Modal.Header>
-      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      <Modal.Body>
+        <div className = "text-center pb-2">
+          Directed by {props.movieData.Director}
+        </div>
+        <div className = "text-center">
+          <Image src={props.movieData.Poster} fluid />
+        </div>
+        <div className = "mt-2">
+          <p><em>{props.movieData.Plot}</em></p>
+        </div>
+      </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={props.handleClose}>
-          Close
-        </Button>
+        <p className = "mr-auto bg-warning"> Rating: {props.movieData.imdbRating}/10</p>
+        <p className = "text-center"> {props.movieData.Released}</p>
       </Modal.Footer>
     </Modal>
   )
